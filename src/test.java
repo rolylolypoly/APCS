dc25dbd0d7f4d190e99b4c119a84bf381289ae87
W Dang <wdang6867@golosal.net>
2015-09-18 22:36:43 -0700

    added returnToOrigin method and tested.

--- src/test.java
+++ src/test.java
@@ -4,7 +4,7 @@ import kareltherobot.*;
  * Created by Will Dang on 9/15/2015.
  */
 
-class BetterBot extends Robot
+class BetterBot extends Robot implements Directions
 {
     public BetterBot (int st, int av, Direction dir, int beeps) {
         super(st, av, dir, beeps);
@@ -15,15 +15,41 @@ class BetterBot extends Robot
         turnLeft();
         turnLeft();
     }
+    public void turnAround() {
+        turnLeft();
+        turnLeft();
+    }
+    //this gets ugly
+    public void returnToOrigin() { //assumes no obstacles
+        while (!facingSouth()) {
+            turnLeft();
+        }
+        if (facingSouth()) {
+            while (frontIsClear()) {
+                move();
+            }
+            if (!frontIsClear()){
+                turnRight();
+                while (frontIsClear()) {
+                    move();
+                }
+            }
+        }
+    }
+
 
 // more commands here …
 
 } // end of the blue print
 public class test implements Directions {
-    public static void main(String[] args) {
-        BetterBot b = new BetterBot(1,1,North, 999);
+    public static void main(String[] args) throws Exception{
+        BetterBot b = new BetterBot(9,9,North, 999);
+        b.returnToOrigin();
+
     }
     static {
-        
+        World.reset();
+        World.readWorld("C:/Users/jacky/Documents/Karel Shitbot/blank.kwld");
+        World.setDelay(0);
     }
 }
